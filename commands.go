409b6ce5df48276763cbb80c97fc0ccc4616b8b0
cahenrichs <cahenrichs@gmail.com>
2025-11-11 22:04:09 +0000

    03 Added the login, register, and user cmd

--- commands.go
+++ commands.go
@@ -2,6 +2,7 @@ package main
 
 import (
 "fmt"
+"errors"
 )
 
 type command struct {
@@ -10,6 +11,7 @@ type command struct {
 }
 
 type commands struct {
+
 	registeredCommands map[string]func(*state, command) error
 }
 
@@ -24,11 +26,34 @@ func (c *commands) register(name string, f func(*state, command) error) {
 func handlerLogin(s *state, cmd command) error {
     if len(cmd.args) < 1 {
         return fmt.Errorf("usage: %s <username>", cmd.name)
+
+	registeredCommands map[string]func(*State, command) error
+}
+
+func (c *commands) run(s *State, cmd command) error {
+	f, ok := c.registeredCommands[cmd.Name]
+	if !ok {
+		return errors.New("command not found")
+	}
+	return f(s, cmd)
+}
+
+func (c *commands) register(name string, f func(*State, command) error) {
+	c.registeredCommands[name] = f
+}
+
+func handlerLogin(s *State, cmd command) error {
+    if len(cmd.Args) < 1 {
+        return fmt.Errorf("usage: %s <username>", cmd.Name)
+
     }
     if s.cfg == nil {
         return fmt.Errorf("state config not initialized")
     }
+
     username := cmd.args[0]
+
+    username := cmd.Args[0]
 
     if err := s.cfg.SetUser(username); err != nil {
         return fmt.Errorf("could not set current user: %w", err)
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 "github.com/cahenrichs/Gator/internal/config"
 "log"
 "fmt"
+"os"
 )
 
 type State struct {
@@ -16,6 +17,7 @@ func main() {
 		log.Fatalf("error reading config: %v", err)
 	}
 
+
 	cfg.SetUser("Clint")
 	if err != nil {
 		return 
@@ -26,4 +28,29 @@ func main() {
 		log.Fatalf("error reading config: %v", err)
 	}
 	fmt.Printf("%+v\n", cfg)
+
+	s := &State{cfg: &cfg}
+
+	cmds := commands{
+		registeredCommands: make(map[string]func(*State, command) error),
+	}
+
+	cmds.register("login", handlerLogin)
+	if len(os.Args) < 2 {
+		fmt.Println("error: not enough arguments")
+		os.Exit(1)
+	}
+	cmdName := os.Args[1]
+	cmdArgs := os.Args[2:]
+
+	cmd := command{
+		Name: cmdName,
+		Args: cmdArgs,
+	}
+
+	if err := cmds.run(s, cmd); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
 }
